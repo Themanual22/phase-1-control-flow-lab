51ecea97d0e79ddb4c1ba4327c4cfa33fbb38422
Themanual22 <djamosry22@gmail.com>
2022-06-13 00:46:42 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 function scuberGreetingForFeet(feet){
   if (feet <= 400) return "This one is on me!";
-  else if (feet < 2500 && feet > 2000) return "I will gladly take your thirty bucks.";
-  else if (feet > 2500) return "No can do.";
+   else if (feet < 2500 && feet > 2000) 
+    return "I will gladly take your thirty bucks.";
+   else if (feet > 2500) 
+    return "No can do.";
 }
 
 
 function ternaryCheckCity(city){
 return (city === "NYC" ? "Ok, sounds good." : "No go.")
 }
-// function switchOnCharmFromTip(tip){
-// switch(tip) {
-//   case 'generous' : 
-//   return 'Thank you so much.';
-//   case 'Not so generous' : 
-//   return "Thank you.";
-//   default : 
-//   return 'Bye.';
-// }}
+
 function switchOnCharmFromTip (tip) {
   switch (tip) {
     case 'generous':
